35f685244158c46da65148e19510970039b6abab
JStanleyBris <jackstanley@nhs.net>
2025-03-25 11:30:33 +0000

    Added in tendon disorders

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -6,7 +6,7 @@
 ######################################
 
 from ehrql import (create_dataset, codelist_from_csv)
-from ehrql.tables.tpp import patients, medications, practice_registrations, addresses
+from ehrql.tables.tpp import patients, medications, practice_registrations, addresses, clinical_events
 from codelists import *
 
 ##Q for Will/Rose - ehrql.tables.tpp vs core(?)
@@ -17,23 +17,32 @@ dataset = create_dataset()
 start_date = "2010-12-01" ##TBC
 end_date = "2024-08-01"  ##TBC
 
+##Need to ensure dmd codes in codelists that I have made
 
 amoxicillin_codes = codelist_from_csv("codelists/opensafely-amoxicillin-oral.csv", column = "code")
 amox_clavulanicacid_codes = codelist_from_csv("codelists/opensafely-co-amoxiclav-oral.csv", column = "code")
 cefalexin_codes = codelist_from_csv("codelists/opensafely-cefalexin-oral.csv", column = "code")
 trimethoprim_codes = codelist_from_csv("codelists/opensafely-trimethoprim.csv", column = "code")
+trim_sulfa_codes = codelist_from_csv("codelists/user-jacklsbrist-trimethoprimsulfamethoxazole-dmd.csv", column = "code")
+
+fluoroquinolone_codes = codelist_from_csv("codelists/user-jacklsbrist-fluoroquinolones-dmd.csv", column = "code")
+
+#Outcome codes
+
+tendinitis_codes = codelist_from_csv("codelists/user-jacklsbrist-tendinitis.csv", column = "code")
 
 has_any_studyabx_prescription = medications.where(
         medications.dmd_code.is_in(amoxicillin_codes)|
         medications.dmd_code.is_in(cefalexin_codes)|
         medications.dmd_code.is_in(amox_clavulanicacid_codes)|
-        medications.dmd_code.is_in(trimethoprim_codes)
+        medications.dmd_code.is_in(trimethoprim_codes)|
+        medications.dmd_code.is_in(trim_sulfa_codes)|
+        medications.dmd_code.is_in(fluoroquinolone_codes)
 ).where(
         medications.date.is_on_or_between(start_date, end_date)
 ).exists_for_patient()
 
 
-#Q for Will/Rose. Here specifying column - is this what we will use to check if they have been prescribed it?
 
 index_date = "2024-03-31"
 
@@ -53,3 +62,25 @@ dataset.date_of_birth = patients.date_of_birth #Likely to need to calculate age 
 dataset.imd = addresses.for_patient_on(index_date).imd_rounded
 patient_address = addresses.for_patient_on(index_date)
 dataset.imd_decile = patient_address.imd_decile
+
+#Medication options
+
+#This extracts first date of FQ prescription
+dataset.first_fluoroquinolone_date = medications.where(
+        medications.dmd_code.is_in(fluoroquinolone_codes)
+).where(
+        medications.date.is_on_or_after(start_date)
+).sort_by(
+        medications.date
+).first_for_patient().date
+
+#Outcome options - ICD-10 or SNOMED - any benefit to either cf the other?
+
+dataset.first_tendinitis_diagnosis_date = clinical_events.where(
+        clinical_events.snomedct_code.is_in(tendinitis_codes)
+).where(
+        clinical_events.date.is_on_or_after(start_date)
+).sort_by(
+        clinical_events.date
+).first_for_patient().date
+
