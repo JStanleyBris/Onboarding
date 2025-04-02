126ea9ad38a68056870ab4e5d7840ba161bbfd16
JStanleyBris <jackstanley@nhs.net>
2025-04-02 12:45:05 +0000

    rose/jaidip_assistance

--- analysis/measure_definition.py
+++ analysis/measure_definition.py
@@ -6,8 +6,10 @@ measures = create_measures()
 
 # Disable disclosure control for demonstration purposes.
 # Values will neither be suppressed nor rounded.
-measures.configure_disclosure_control(enabled=False)
+measures.configure_disclosure_control(enabled=False) #change to true when running. Ok as false for now
 measures.configure_dummy_data(population_size=1000)
+
+#De novo dataset generation for measures here - but could make external file for queries that would be shared by both
 
 # Codelist - do we need to redo this or can we link it to the dataset_definition?
 
@@ -26,10 +28,12 @@ fluoroquinolone_rx = rx_in_interval.where(
 
 measures.define_measure(
     name="fluoroquinolone_trends",
-    numerator= fluoroquinolone_rx.exists_for_patient(),
-    denominator=patients.exists_for_patient(), #eg all patients - is it possible to make this all patients registered at that timepoint? Eg /
-                                                #patients.practice_registrations.for_patient_on(INTERVAL).exists_for_patient()
+    numerator= fluoroquinolone_rx.exists_for_patient(), #countforpatient(?) - to consider
+    denominator=practice_registrations.spanning(INTERVAL.start_date, INTERVAL.end_date).exists_for_patient(), #eg all patients - is it possible to make this all patients registered at that timepoint? Eg /
+                                                #practice_registrations.for_patient_on(INTERVAL).exists_for_patient() - 
+                                                #  ?practice registrations is own table
     intervals=months(120).starting_on("2010-12-01"), #Q - can this be linked to start_date in dataset_definition?
-    #Q2 - can we make this 3 months (or 365.25/4 days?)
+    #Q2 - can we make this 3 months (or 365.25/4 days?) - input start_date as a runtime argument and supply it the action 
+    #Make the variable - in the same file as shared queries put shared variables - put this in analysis
 
 )
