90c0d171e3337ab42aba395a2179c7469137449d
JStanleyBris <jackstanley@nhs.net>
2025-05-19 12:33:01 +0000

    Expanded_measure

--- analysis/measure_definition.py
+++ analysis/measure_definition.py
@@ -10,12 +10,21 @@ measures.configure_dummy_data(population_size=1000)
 
 #De novo dataset generation for measures here - but could make external file for queries that would be shared by both
 
-#Exposures
+#Exposures - fq
 fluoroquinolone_codes = codelist_from_csv("codelists/user-jacklsbrist-fluoroquinolones-dmd.csv", column = "code")
+
+#Exposures - comparators
 amoxicillin_codes = codelist_from_csv("codelists/opensafely-amoxicillin-oral.csv", column = "code")
+cefalexin_codes = codelist_from_csv("codelists/opensafely-cefalexin-oral.csv", column = "code")
+co_amox_codes = codelist_from_csv("codelists/opensafely-co-amoxiclav-oral.csv", column = "code")
+trim_codes = codelist_from_csv("codelists/opensafely-trimethoprim.csv", column = "code")
+co_trim_codes = codelist_from_csv("codelists/user-jacklsbrist-trimethoprimsulfamethoxazole-dmd.csv", column = "code")
+
+all_comparator_abx = amoxicillin_codes + cefalexin_codes + co_amox_codes +trim_codes + co_trim_codes
 
 #Outcomes
 tendinitis_codes = codelist_from_csv("codelists/user-jacklsbrist-tendinitis.csv", column = "code")
+neuropathy_codes = codelist_from_csv("codelists/user-jacklsbrist-peripheral-neuropathy.csv", column = "code")
 
 # The use of the special INTERVAL placeholder below is the key part of
 # any measure definition as it allows the definition to be evaluated
@@ -31,92 +40,146 @@ dx_in_interval = clinical_events.where(
 #Define durations - saves repetition
 measures.define_defaults(intervals=months(120).starting_on("2010-12-01"))
 
-#Define numerators - abx
+    #Define numerators - abx
 
-fluoroquinolone_rx = rx_in_interval.where(
-    medications.dmd_code.is_in(fluoroquinolone_codes)
-    )
+#FQ
+fluoroquinolone_rx = rx_in_interval.where(medications.dmd_code.is_in(fluoroquinolone_codes))
 
-amoxicillin_rx = rx_in_interval.where(
-    medications.dmd_code.is_in(amoxicillin_codes)
-)
+#Comparators
+amoxicillin_rx = rx_in_interval.where(medications.dmd_code.is_in(amoxicillin_codes))
+cefalexin_rx = rx_in_interval.where(medications.dmd_code.is_in(cefalexin_codes))
+co_amox_rx = rx_in_interval.where(medications.dmd_code.is_in(co_amox_codes))
+trim_rx = rx_in_interval.where(medications.dmd_code.is_in(trim_codes))
+co_trim_rx = rx_in_interval.where(medications.dmd_code.is_in(co_trim_codes))
 
-#Define numerators - outcomes
+    #Define numerators - outcomes
 
-tendinitis_dx = dx_in_interval.where(
-    clinical_events.snomedct_code.is_in(tendinitis_codes)
-)
+tendinitis_dx = dx_in_interval.where(clinical_events.snomedct_code.is_in(tendinitis_codes))
+neuropathy_dx = dx_in_interval.where(clinical_events.snomedct_code.is_in(neuropathy_codes))
 
-#Define numerators - outcomes with antibiotic prescription in previous 30d - ?dummy data - suggested by Rose
+    #Challenge of looking at outcome post antibiotic prescription - first get date of outcome. Then look 30days back for prescription
 
-#tendinitis_prev_amox_dx = tendinitis_dx.where(
- #   amoxrx.date.is_on_or_between(
-  #      tendinitis_dx.date - days(30), tendinitis_dx.date - days(1)).exists_for_patient()
-#)
+#Outcome date
 
 first_tendinitis = clinical_events.where(
     clinical_events.snomedct_code.is_in(tendinitis_codes)).where(
-        clinical_events.date.is_during(INTERVAL)).sort_by(clinical_events.date).first_for_patient()
-    
-.date
-amoxrx = medications.where(medications.dmd_code.is_in(amoxicillin_codes)).where(
-        medications.date.is_on_or_between(first_tendinitis - days(30), first_tendinitis - days(1))
+        clinical_events.date.is_during(INTERVAL)).sort_by(clinical_events.date).first_for_patient().date
+
+first_neuropathy = clinical_events.where(
+    clinical_events.snomedct_code.is_in(neuropathy_codes)).where(
+        clinical_events.date.is_during(INTERVAL)).sort_by(clinical_events.date).first_for_patient().date
+
+#Rx 30d before outcome
+
+tendinitis_window_start = first_tendinitis - days(30)
+tendinitis_window_end = first_tendinitis - days(1)
+
+neuropathy_window_start = first_neuropathy - days(30) 
+neuropathy_window_end = first_neuropathy - days(1)
+
+
+comparator_rx_pre_tendinitis = medications.where(medications.dmd_code.is_in(all_comparator_abx)).where(
+        medications.date.is_on_or_between(tendinitis_window_start, tendinitis_window_end)
                                           )
 
-#first_tendinitis= clinical_events.where(
- #   clinical_events.snomedct_code.is_in(tendinitis_codes)).where(
-  #      clinical_events.date.is_on_or_between(amoxrx + days(1), amoxrx + days(30))
-#)
+fluoroquinolone_rx_pre_tendinitis = medications.where(medications.dmd_code.is_in(fluoroquinolone_codes)).where(
+        medications.date.is_on_or_between(tendinitis_window_start, tendinitis_window_end)
+                                          )
 
-#D/w Rose 9/4 - show option helpful for understanding 'hidden' data processing etc
+comparator_rx_pre_neuropathy = medications.where(medications.dmd_code.is_in(all_comparator_abx)).where(
+        medications.date.is_on_or_between(neuropathy_window_start, neuropathy_window_end)
+                                          )
 
-#tendinitis_post_amox = clinical_events.where(
- #   clinical_events.snomedct_code.is_in(tendinitis_codes)
-  #  .where(medications.dmd_code.is_in(amoxicillin_codes))
-   #        .where(medications.date.is_on_or_between(clinical_events.date - days(30), clinical_events.date - days(1)))
-#)
+fluoroquinolone_rx_pre_neuropathy = medications.where(medications.dmd_code.is_in(fluoroquinolone_codes)).where(
+        medications.date.is_on_or_between(neuropathy_window_start, neuropathy_window_end)
+                                          )                                         
 
-#tendinitis_post_amox = rx_in_interval.where(
- #   medications.dmd_code.is_in(amoxicillin_codes)
-#).where(
- #   medications.date.is_on_or_between(first_tendinitis_diagnosis_date - days(30), first_tendinitis_diagnosis_date - days(1))
-#)
-
-
-
-#Define denominators
+    #Define denominators
 
 denominator_abxcount = (
-    practice_registrations.spanning(INTERVAL.start_date, INTERVAL.end_date).exists_for_patient()
+    practice_registrations.spanning(INTERVAL.start_date, INTERVAL.end_date)
     )
 
-denominator_all_amox = ( #For use with post abx outcomes
- medications.where(medications.dmd_code.is_in(amoxicillin_codes))
- .where(medications.date.is_on_or_between(INTERVAL.start_date, INTERVAL.end_date))
+denominator_all_comparator = ( #For use with post abx outcomes - this is count of all prescriptions within the timeframe - look back 30 days
+ medications.where(medications.dmd_code.is_in(all_comparator_abx))
+ .where(medications.date.is_on_or_between(INTERVAL.start_date - days(30), INTERVAL.end_date - days(30)))
+)
+
+denominator_fq_specific = ( #For use with post abx outcomes - this is count of all prescriptions within the timeframe - look back 30 days
+ medications.where(medications.dmd_code.is_in(fluoroquinolone_codes))
+ .where(medications.date.is_on_or_between(INTERVAL.start_date - days(30), INTERVAL.end_date - days(30)))
 )
 
 #Start with measures
 
+#SImple abx trend  - use count_for_patient to account for repeat prescribing in 30d - likely minimal
 measures.define_measure(
     name="fluoroquinolone_trends",
     numerator= fluoroquinolone_rx.count_for_patient(), #this runs and works - and only counts by patient within interval I think because of rx_in_interval
-    denominator=denominator_abxcount, 
+    denominator=denominator_abxcount.exists_for_patient(), 
 )
 
-measures.define_measure( #Is this the best way to work with measures here - to make multiple measures
-#Or would I be better off using group_by? If so would I have to make a new group
+measures.define_measure(
     name="amoxicillin_trends",
     numerator= amoxicillin_rx.count_for_patient(),
-    denominator=denominator_abxcount,
+    denominator=denominator_abxcount.exists_for_patient(),
 )
+
+measures.define_measure(
+    name="cefalexin_trends",
+    numerator= cefalexin_rx.count_for_patient(),
+    denominator=denominator_abxcount.exists_for_patient(),
+)
+
+measures.define_measure(
+    name="trim_trends",
+    numerator= trim_rx.count_for_patient(),
+    denominator=denominator_abxcount.exists_for_patient(),
+)
+
+measures.define_measure(
+    name="co_trim_trends",
+    numerator= co_trim_rx.count_for_patient(),
+    denominator=denominator_abxcount.exists_for_patient(),
+)
+
+#Simple AE trend - use exist_for_patient to restrict to 1 occurence per person
 measures.define_measure(
     name="tendinitis_trends",
     numerator= tendinitis_dx.exists_for_patient(), #exists better than count here - any rpt coding would be same dx
-    denominator = denominator_abxcount,
+    denominator = denominator_abxcount.exists_for_patient(),
 )
 
 measures.define_measure(
-    name="tendinitis_prevamox_trends",
-    numerator= amoxrx.exists_for_patient(), #this runs and works - and only counts by patient within interval I think because of rx_in_interval
-    denominator=denominator_abxcount, 
+    name="neuropathy_trends",
+    numerator= neuropathy_dx.exists_for_patient(), 
+    denominator = denominator_abxcount.exists_for_patient(),
+)
+
+    #Outcome post abx
+
+#Tendinitis
+measures.define_measure(
+    name="tendinitis_prev_comparator_trends",
+    numerator= comparator_rx_pre_tendinitis.exists_for_patient(), #this runs and works - and only counts by patient within interval I think because of rx_in_interval
+    denominator= denominator_all_comparator.exists_for_patient(), 
+)
+
+measures.define_measure(
+    name="tendinitis_prev_fluoroquinolone_trends",
+    numerator= fluoroquinolone_rx_pre_tendinitis.exists_for_patient(), 
+    denominator= denominator_fq_specific.exists_for_patient(), 
+)
+
+#Neuropathy
+measures.define_measure(
+    name="neuropathy_prev_comparator_trends",
+    numerator= comparator_rx_pre_neuropathy.exists_for_patient(), 
+    denominator= denominator_all_comparator.exists_for_patient(), 
+)
+
+measures.define_measure(
+    name="neuropathy_prev_fluoroquinolone_trends",
+    numerator= fluoroquinolone_rx_pre_neuropathy.exists_for_patient(),
+    denominator= denominator_fq_specific.exists_for_patient(), 
 )
