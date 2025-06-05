dd3a8fb17b5a897aad86835c5e75aba372dca6aa
JStanleyBris <jackstanley@nhs.net>
2025-06-05 13:26:51 +0000

    unadjcox_update

--- analysis/unadj_coxph.R
+++ analysis/unadj_coxph.R
@@ -9,7 +9,7 @@ df <- readr::read_csv("output/dataset.csv.gz", col_types = readr::cols(
     first_fluoroquinolone_date = col_date(format = "%Y-%m-%d"),
     first_tendinitis_diagnosis_date = col_date(format = "%Y-%m-%d"),
     first_neuropathy_diagnosis_date = col_date(format = "%Y-%m-%d"),
-    daterx = col_date(format = "%Y-%m-%d"),
+    date_cohort_prescription = col_date(format = "%Y-%m-%d"),
     date_of_death = col_date(format = "%Y-%m-%d"),
     coamox_exp = col_logical(),
     fluoroquinolone_exp = col_logical()
@@ -19,7 +19,7 @@ df <- readr::read_csv("output/dataset.csv.gz", col_types = readr::cols(
 df <- df %>%
 mutate(
 # Calculate 30-day censoring date
-    censor_30d = date_rx + days(30),
+    censor_30d = date_cohort_prescription + days(30),
 # Compute the censoring date: the earliest of event, death, or 30-day censor
     censor_date = pmin(first_tendinitis_diagnosis_date, date_of_death, censor_30d, na.rm = TRUE),
       # Create event indicator: 1 if event occurred on or before censoring, 0 otherwise
@@ -28,7 +28,7 @@ mutate(
       1, 0
     ),
     # Time from entry to censoring (in days)
-    time = as.numeric(difftime(censor_date, date_rx, units = "days"))
+    time = as.numeric(difftime(censor_date, date_cohort_prescription, units = "days"))
 )
 
 summary(df$time)
